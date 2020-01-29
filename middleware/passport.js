661447e3779cfa9135ac79f2f3d70da9f36417bf
lpolon <lpolon@gmail.com>
2020-01-29 17:47:26 -0300

    removed unecessary passport changes

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,21 +1,4 @@
-const passport = require('passport');
+// const passport = require('passport');
 
-// middleware para rotas prote
-
-const signupStrat = passport.authenticate('local-signup', {
-  successRedirect: '/login',
-  failureRedirect: '/signup',
-  passReqToCallback: true,
-});
-
-const loginStrat = passport.authenticate('local-login', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  passReqToCallback: true,
-});
-
-module.exports = {
-  signupStrat,
-  loginStrat,
-};
-    
+// module.exports = {
+// };
