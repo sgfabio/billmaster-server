94137c3f4d083093758c767dda0739df93e0335a
lpolon <lpolon@gmail.com>
2020-01-29 18:32:45 -0300

    added required to owner field

--- models/Groups.js
+++ models/Groups.js
@@ -4,7 +4,7 @@ const { Schema, model } = mongoose;
 const groupSchema = new Schema(
   {
     groupName: String,
-    owner: { type: Schema.Types.ObjectId, ref: 'user' },
+    owner: { type: Schema.Types.ObjectId, ref: 'user', required: true },
     members: [{ type: Schema.Types.ObjectId, ref: 'member' }],
     description: String,
     total: Number,
