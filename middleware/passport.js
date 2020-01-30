3c26f40ddcd0130f4b9765e43193ec2d7b6fafd9
lpolon <lpolon@gmail.com>
2020-01-29 22:18:00 -0300

    removed unnecessary passport cfg lines from app.js since it was used in a separete config file - nice! i named the anonymous function wrapping passport.authenticate() as 'loginHandler' to make it easier to read and change the code in the future.

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,4 +1,36 @@
-// const passport = require('passport');
+const passport = require('passport');
 
-// module.exports = {
-// };
+/*
+um uma função do tipo middleware chamado handleLogin está retornando passport.authenticate(); isso é uma Immediately Invoked Function Expression (IIFE). A função disso é disponibilizar o req, res e next para o passport.authenticate()
+https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+*/
+const loginHandler = (req, res, next) => {
+  passport.authenticate('local', (err, user, failureDetails) => {
+    if (err) {
+      res
+      .status(500)
+      .json({ message: 'Something went wrong authenticating user' });
+      return;
+    }
+    
+    if (!user) {
+      // "failureDetails" contains the error messages
+      // from our logic in "LocalStrategy" { message: '...' }.
+      res.status(401).json(failureDetails);
+      return;
+    }
+    
+    // save user in session
+    req.login(user, (err) => {
+      if (err) {
+        res.status(500).json({ message: 'Session save went bad.' });
+        return;
+      }
+      
+      // We are now logged in (that's why we can also send req.user)
+      res.status(200).json(user);
+    });
+  })(req, res, next);
+};
+
+module.exports = loginHandler;
