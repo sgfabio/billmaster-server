e8057efa6c9c4fdbd620911009df4f53a07fdcf6
lpolon <lpolon@gmail.com>
2020-01-31 15:00:54 -0300

    expense POST now working

--- routes/expenses.js
+++ routes/expenses.js
@@ -1,10 +1,47 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
-router.post('/despesa', (req, res, next) => {
-  // receber uma nova despesa de um grupo;
-  const { groupId, description, value, splitObj } = req.body;
-  const { paidBy, dividedByArr } = splitObj;
+const Expense = require('../models/Expense');
+const Group = require('../models/Groups');
+
+router.post('/', (req, res, next) => {
+  const {
+    description,
+    value,
+    split: { paidBy, dividedBy },
+  } = req.body;
+
+  Expense.create({
+    group: req.groupId,
+    description,
+    value,
+    split: {
+      paidBy,
+      dividedBy,
+    },
+  })
+    .then((response) =>
+      Group.findByIdAndUpdate(req.groupId, { $push: { expenses: response } })
+    )
+    .then((response) =>
+      res.status(201).json({
+        msg: `expense with value of ${response.value} created sucessfully`,
+        newExpense: response,
+      })
+    )
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
 });
+
+// router.param('expenseId', (req, res, next, expenseIdParam) => {
+//   if (!mongoose.Types.ObjectId.isValid(expenseIdParam)) {
+//     res.status(400).json({ msg: 'invalid expenseId!' });
+//     return;
+//   }
+//   req.expenseId = expenseIdParam;
+//   next();
+// });
 
 module.exports = router;
