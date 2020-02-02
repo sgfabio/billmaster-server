5a6360238b626eff15da53f71959ec1db5dfca95
lpolon <lpolon@gmail.com>
2020-02-02 19:53:21 -0300

    mongoose setups to deal with DeprecationWarnings

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ require('dotenv').config();
 const session = require('express-session');
 const bodyParser = require('body-parser');
 
+// setup to deal with DeprecationWarning:
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then((resolvedProm) => {
     console.log(
       `Connected to Mongo! Database name: "${resolvedProm.connections[0].name}"`
